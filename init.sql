8fa5a69aa67ce7b70f83c1164398cf7cbc0f88ad
Jhonathan.S <jhonathan_silva93@hotmail.com>
2025-07-24 19:07:13 -0300

    finalizado

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `cbc`;
+CREATE DATABASE IF NOT EXISTS `cbc`;
 
 CREATE TABLE `cbc`.`clube` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Identificador único do clube (PK)',
@@ -13,6 +13,8 @@ CREATE TABLE `cbc`.`recurso` (
     `saldo_disponivel` DECIMAL(15,2) NOT NULL DEFAULT 0.00 COMMENT 'Valor disponível do recurso',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Controle de recursos financeiros do sistema';
+
+TRUNCATE TABLE `cbc`.`recurso`;
 
 INSERT INTO `cbc`.`recurso` (`recurso`, `saldo_disponivel`) VALUES
 ('Recurso para passagens', 10000.00),
